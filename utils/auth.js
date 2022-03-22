a8d446392e328fcb7fe96471b7569d0039bf4f8e
TomJia98 <Tomchristian1998@gmail.com>
2022-03-22 11:20:46 +0800

    changed handlebars

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
 const withAuth = (req, res, next) => {
   // TODO: Add a comment describing the functionality of this if statement
   if (!req.session.logged_in) {
-    res.redirect('/login');
+    res.redirect("/login");
   } else {
     next();
   }
